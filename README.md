a3169b185af65cfe756075b8cf4eac52573ff43d
tkruba <tkruba@gmail.com>
2022-09-21 02:31:13 +0200

    Menu de Navigation
    Addition:
    
    + Navigation menu
    + Scrolling from navigation menu
    + Cards stylisation (subject to change)
    + Few animations
    
    Removed:
    
    - Deprecated attributes from the map iframe
    
    Adjusted:
    
    ~ Header size
    ~ Scroll to top button size
    ~ Improved JS script by adding a state function for the navigation menu

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 
 Site web pour la Ferme des Charmes (actuellement https://lafermedescharmes.jimdofree.com/)
 
-## To-Do (mis à jour le 20/09/2022)
+## To-Do (mis à jour le 21/09/2022)
 
 - Stylisation
 
 - Ajout d'images
 
 - Remplacement des LoremIpsum
-
-- Remplissage du menu de navigation
 
 
 ## Langages utilisés
--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 let menuToggle = document.getElementById('menu__toggle');
 let menu = document.getElementById('menu');
 
-menuToggle.addEventListener('click', e => {
-    e.preventDefault();
+const changeMenuState = () => {
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
+};
+
+menuToggle.addEventListener('click', event => {
+    event.preventDefault();
+    changeMenuState();
 });
 
 let scrollToTopBtn = document.getElementById('to__top');
 
-scrollToTopBtn.addEventListener('click', e => {
+scrollToTopBtn.addEventListener('click', event => {
     scrollTo({
         top: 0,
         left: 0,
@@ -31,3 +35,20 @@ const observer = new IntersectionObserver(
 let observed = document.getElementById('about');
 
 observer.observe(observed);
+
+let links = document.getElementsByTagName('a');
+
+Array.prototype.forEach.call(links, el => {
+    let ref = el.getAttribute('href');
+    if (ref && ref.includes('#')) {
+        el.addEventListener('click', event => {
+            event.preventDefault();
+            scrollTo({
+                left: 0,
+                top: document.getElementById(ref.replace(/#/g, '')).getBoundingClientRect().top - (4*16) + window.scrollY,
+                behavior: 'smooth'
+            });
+            changeMenuState();
+        });
+    }
+});
